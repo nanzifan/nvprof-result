b32fbd1b5a3ed8eb91d3dd4ef299e43715942770
znan <znan@ncsu.edu>
2019-06-28 16:25:59 -0700

    modify script to ron nvprof

--- cudastream/190627184259/run-nvprof.sh
+++ cudastream/190627184259/run-nvprof.sh
@@ -17,7 +17,7 @@ commands=(
 
 for i in "${commands[@]}"; do
 	echo $i
-	filename=${$i | cut -d' ' -f 1}
+	filename=$(echo $i|cut -d' ' -f 1)
 	logName="$newFolder/$filename.txt"
 	/usr/local/cuda/bin/nvprof --$i --log-file $logName $1 $2	
 done
