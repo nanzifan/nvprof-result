40dd452f4b671d12a8bf18219820ea453d993606
znan <znan@ncsu.edu>
2019-06-28 14:35:13 -0700

    nvprof script ready for test

---
+++ cudastream/190627184259/run-nvprof.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+logFolder="/home/znan/nvprof-result"
+subFolder=${PWD##*/}
+
+newFolder="$logFolder$subFolder"
+
+mkdir newFolder
+echo "new folder" $newFolder
+
+commands=(
+	"--track-memory-allocations on"
+	"--print-gpu-trace"
+	"--cpu-thread-tracing"
+	"--cpu-profiling"
+	)
+
+for i in "${commands[@]}"; do
+	logName="$newFolder/$i.txt"
+	/usr/local/cuda/bin/nvprof "$i" --log-file $logName $1 $2
+	echo $i
+done
+
+echo 'Done'
