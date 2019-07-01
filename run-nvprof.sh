34d2b16f6837e7e6984fb2ec4146e02e36fee38d
znan <znan@ncsu.edu>
2019-07-01 11:13:49 -0700

    modify script to ron nvprof

--- run-nvprof.sh
+++ run-nvprof.sh
@@ -27,7 +27,7 @@ for i in "${commands[@]}"; do
 	echo $i
 	filename=$(echo $i|cut -d' ' -f 1)
 	logName="$newFolder/$filename.txt"
-	/usr/local/cuda/bin/nvprof --$i --device-buffer-size 128MB --log-file $logName $1 $2	
+	/usr/local/cuda/bin/nvprof --$i --device-buffer-size 1024MB --log-file $logName $1 $2	
 done
 
 echo 'Done'
